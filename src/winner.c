9fb9b1d6f421a209ab2c26302c4ccb92fa1b51f6
Jasper ronald Simonis <jsimonis@f0r6s1.codam.nl>
2022-03-10 14:43:23 +0100

    Replaced (x, y) with (q, r)

--- src/winner.c
+++ src/winner.c
@@ -2,42 +2,42 @@
 
 #include <stdio.h>
 
-static int getLengthX(int x, int y)
+static int getLengthX(int q, int r)
 {
 	int len = 0;
 
-	t_hex piece = gameData.gameGrid[x][y];
-	for (; x < gameData.maxLine; x++)
+	t_hex piece = gameData.gameGrid[q][r];
+	for (; q < gameData.maxLine; q++)
 	{
-		if (gameData.gameGrid[x][y] != piece)
+		if (gameData.gameGrid[q][r] != piece)
 			break;
 		len++;
 	}
 	return len;
 }
 
-static int getLengthY(int x, int y)
+static int getLengthY(int q, int r)
 {
 	int len = 0;
 
-	t_hex piece = gameData.gameGrid[x][y];
-	for (; y < gameData.maxLine; y++)
+	t_hex piece = gameData.gameGrid[q][r];
+	for (; r < gameData.maxLine; r++)
 	{
-		if (gameData.gameGrid[x][y] != piece)
+		if (gameData.gameGrid[q][r] != piece)
 			break;
 		len++;
 	}
 	return len;
 }
 
-static int getLengthXY(int x, int y)
+static int getLengthXY(int q, int r)
 {
 	int len = 0;
 
-	t_hex piece = gameData.gameGrid[x][y];
-	for (; x < gameData.maxLine && y < gameData.maxLine; x++, y++)
+	t_hex piece = gameData.gameGrid[q][r];
+	for (; q < gameData.maxLine && r < gameData.maxLine; q++, r++)
 	{
-		if (gameData.gameGrid[x][y] != piece)
+		if (gameData.gameGrid[q][r] != piece)
 			break;
 		len++;
 	}
@@ -69,22 +69,22 @@ t_player getWinner()
 	int maxLengths[2] = { 0, 0 };
 	int counts[2] = { 0, 0 };
 
-	for (int x = 0; x < gameData.maxLine; x++)
-		for (int y = 0; y < gameData.maxLine; y++)
+	for (int q = 0; q < gameData.maxLine; q++)
+		for (int r = 0; r < gameData.maxLine; r++)
 		{
-			if (!areCoordinatesValid(x, y))
+			if (!areCoordinatesValid(q, r))
 				continue;
-			t_hex piece = gameData.gameGrid[x][y];
+			t_hex piece = gameData.gameGrid[q][r];
 			if (piece == EMPTY)
 				continue;
 			t_player playerForPiece = (piece - 1) / 2;	// TODO: use a getPlayerForHex(t_hex) function
-			addLength(getLengthX(x, y), &maxLengths[playerForPiece], &counts[playerForPiece]);
-			addLength(getLengthY(x, y), &maxLengths[playerForPiece], &counts[playerForPiece]);
-			addLength(getLengthXY(x, y), &maxLengths[playerForPiece], &counts[playerForPiece]);
+			addLength(getLengthX(q, r), &maxLengths[playerForPiece], &counts[playerForPiece]);
+			addLength(getLengthY(q, r), &maxLengths[playerForPiece], &counts[playerForPiece]);
+			addLength(getLengthXY(q, r), &maxLengths[playerForPiece], &counts[playerForPiece]);
 		}
 
 	int					winner = compare(maxLengths[PLAYER_ONE], maxLengths[PLAYER_TWO]);	// Choose the one with the longest length
-	if (winner == 0)	winner = compare(counts[PLAYER_ONE],     counts[PLAYER_TWO]    );	// If its a tie, choose the one with the most number of said length
+	if (winner == 0)	winner = compare(    counts[PLAYER_ONE],     counts[PLAYER_TWO]);	// If its a tie, choose the one with the most number of said length
 	
 	// winner == -1:	PLAYER_TWO wins
 	// winner == 0:		NONE wins
